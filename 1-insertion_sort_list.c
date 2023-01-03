3dd1a859fd835e858b7790c0a69a89bc094b1fd7
YuranyUlchur <ulchuryurani80@gmail.com>
2023-01-03 16:52:18 -0500

    add new file

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,9 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - function that sorts a doubly linked list
+ * @list: The array
+ */
+void insertion_sort_list(listint_t **list)
+{
+}
--- sort.h
+++ sort.h
@@ -22,5 +22,6 @@ typedef struct listint_s
 void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+void insertion_sort_list(listint_t **list);
 
 #endif
