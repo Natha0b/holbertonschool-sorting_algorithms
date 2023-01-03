86a0dbe608541af2e9d00f62ca35007b282af9d3
Natha0b <laura.barona04@gmail.com>
2023-01-03 14:58:44 -0500

    create the funciont bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,11 +1,25 @@
 #include "sort.h"
 
 /**
- * bubble_sort - simple sorting algorithm 
+ * bubble_sort - simple sorting algorithm
  * @array: The array
  * @size: Number of elements in @array
  */
 void bubble_sort(int *array, size_t size)
 {
+	unsigned int step, i, temp;
 
+	for (step = 0; step < size - 1; ++step)
+	{
+
+		for (i = 0; i < size - step - 1; ++i)
+		{
+			if (array[i] > array[i + 1])
+			{
+				temp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
+			}
+		}
+	}
 }
