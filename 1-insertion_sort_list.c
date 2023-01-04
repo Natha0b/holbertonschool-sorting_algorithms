b272c4c75636e9f5c3ea8368575bdddaf245cc66
YuranyUlchur <ulchuryurani80@gmail.com>
2023-01-03 20:51:20 -0500

    add function that sorts a doubly linked list

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -2,23 +2,65 @@
 
 /**
  * insertion_sort_list - function that sorts a doubly linked list
- * @list: The array
+ * @list: doubly linked list
  */
 void insertion_sort_list(listint_t **list)
 {
-	unsigned int i;
-	listint_t *temp;
+	listint_t *header;
 
-	if (list != NULL)
+	if (list == NULL || *list == NULL)
+		return;
+
+	header = *list;
+	while (header && header->next)
 	{
+		if (header->n > header->next->n)
+		{
+			_swap(&header, list);
+			print_list(*list);
 
-		for (i = 0; list[i] != NULL; ++i)
-			if (list[i] > list[i + 1])
+			while (header && header->prev)
 			{
-				temp = list[i];
-				list[i] = list[i + 1];
-				list[i + 1] = temp;
-				print_list(temp);
+
+				if (header->n < header->prev->n)
+				{
+					header = header->prev;
+
+					_swap(&(header), list);
+
+					print_list(*list);
+					header = header->next;
+				}
+				header = header->prev;
 			}
+		}
+		header = header->next;
 	}
+}
+/**
+ * _swap - function two nodes of doubly linked list
+ * @list: varibale listint_t
+ * @node: varibale listint_t
+ */
+void _swap(listint_t **node, listint_t **list)
+{
+	listint_t *temp = *node, *temp2, *temp3;
+
+	if (!(*node)->prev)
+		*list = (*node)->next;
+
+	temp2 = temp->next;
+	temp->next = temp2->next;
+	temp3 = temp->prev;
+	temp->prev = temp2;
+	temp2->next = temp;
+	temp2->prev = temp3;
+
+	if (temp2->prev)
+		temp2->prev->next = temp2;
+
+	if (temp->next)
+		temp->next->prev = temp;
+
+	*node = temp2;
 }
--- sort.h
+++ sort.h
@@ -23,5 +23,6 @@ void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void insertion_sort_list(listint_t **list);
+void _swap(listint_t **node, listint_t **list);
 
 #endif
