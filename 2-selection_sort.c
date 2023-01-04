14d1acad3f7557b877f2ff0e40d48ac6944ca6fa
YuranyUlchur <ulchuryurani80@gmail.com>
2023-01-04 09:37:54 -0500

    changes the variable

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -7,28 +7,25 @@
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, min_idx;
+	size_t i, j, idx;
 	int aux;
 
 	if (array == NULL || size < 1)
-	{
 		return;
-	}
-
 	for (i = 0; i < size - 1; i++)
 	{
-		min_idx = i;
+		idx = i;
 		for (j = i + 1; j < size; j++)
 		{
-			if (array[j] < array[min_idx])
-				min_idx = j;
+			if (array[j] < array[idx])
+				idx = j;
 		}
 
-		if (min_idx != i)
+		if (idx != i)
 		{
 			aux = array[i];
-			array[i] = array[min_idx];
-			array[min_idx] = aux;
+			array[i] = array[idx];
+			array[idx] = aux;
 			print_array(array, size);
 		}
 	}
